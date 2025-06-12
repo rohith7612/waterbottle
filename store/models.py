bc030e0e1b656f44474efbadb5a0d78d1645bbc1
rohith7612 <prudhvirohith45@gmail.com>
2025-06-12 13:52:25 +0530

    commit

--- store/models.py
+++ store/models.py
@@ -13,7 +13,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='product_images/')
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='Water Bottle')
 
     def __str__(self):
